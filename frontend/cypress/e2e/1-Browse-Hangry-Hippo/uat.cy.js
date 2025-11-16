16910479f0ff24b7366c03dc29ac7ede76355ad2
Mihai Chindris <chindris-mihai-alexandru@users.noreply.github.com>
2025-11-16 23:50:05 +0200

    Implement UAT tests for category selection with assertions

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,15 @@
-describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+describe("Category Selection Tests", () => {
+  beforeEach(() => {
+    cy.visit("https://hangryhippo.quantic.host/");
   });
-  it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+
+  it("selects Handhelds", () => {
+    cy.contains("Handhelds").click();
+    cy.contains("Cheese Burger").should("be.visible");
+    cy.contains("Fajita Tacos").should("be.visible");
   });
-  it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+
+  it("selects Appeteasers", () => {
+    cy.contains("Appeteasers").click();
   });
 });
